47b213323e2d66c03ba08580ed2b532dd6340537
kibaara-wangu <kibaarawanguggmail.com>
2024-03-07 21:00:48 -0500

    added files

---
+++ index.js
@@ -0,0 +1,17 @@
+var a = 3;
+console.log(a);
+var a =5;
+console.log(a);
+let b = 6;
+console.log(b);
+b = 3
+console.log(b);
+
+
+
+const c = 2;
+console.log(c)
+let person;
+console.log(person)
+person = "Jane";
+console.log('person 2:',person);
---
+++ operators.js
@@ -0,0 +1,59 @@
+let a =20;
+let b = 30;
+// Arithmetic
+let add = a + b;
+console.log({ add });
+
+let subtract = a -b;
+console.log({subtract});
+
+let multiply = a * b;
+console.log({multiply});
+
+let division = a / b;
+console.log({division});
+// Comparison
+let c = '20';
+let looselyEqualTo = a ==c;
+console.log({ looselyEqualTo});
+
+let strictlyEqualTo = a ===c;
+console.log({strictlyEqualTo});
+
+
+
+let looselyNotEqualTo = a !=c;
+console.log({looselyNotEqualTo});
+
+let strictlyNotEqualTo = a !==c;
+console.log({strictlyNotEqualTo});
+// Assignment
+a+=5;
+console.log({a});
+// Decrement
+a--;
+console.log({a});
+
+// Increment
+a++;
+console.log({a});
+
+// Implicit coertion
+console.log({c});
+let d = +c;
+console.log({d});
+
+let e = c * 1;
+console.log({e})
+
+// Explicit coersion
+let f =Number(c);
+console.log({f});
+
+
+// Strings
+
+
+
+
+
